997b25303c4f167fff3d3275a2eeeeb36f4b9f74
vivin <octaviavivin286@gmail.com>
2023-10-20 14:36:33 +0700

    init

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(scaffoldBackgroundColor: Colors.white),
+      theme: ThemeData(scaffoldBackgroundColor: Colors.pink),
       routes: {
         "/": (context) => Utama(),
         // "/profile" : (context) => ),
